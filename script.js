8e1620f4eb59cd9625ae41707991f4248dc9f47c
Guntur Santhosh Kumar <98687152+santhosh1997g@users.noreply.github.com>
2023-01-17 20:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-}
+// This file is not to be modified. Please ignore this.
+// We will understand all of this later in the course.
+// DO NOT MODIFY THIS FILE
 
-module.exports = letterCombinations;
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const letterCombination = require('./script');
+
+app.use(express.static(__dirname))
+
+app.post('/lettercomb',(req, res) => {
+  const input = req.body.num
+  const answer = letterCombination(input) 
+  res.send({message:JSON.stringify(answer)})
+})
+
+module.exports = app;
